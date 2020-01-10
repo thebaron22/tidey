e5b264d84f7dc061ab1d25fad85754b6719fe9d5
Mick <the8aron@yahoo.com>
2020-01-10 13:49:14 +1100

    first sunset

--- Tidey.pl
+++ Tidey.pl
@@ -1,10 +1,19 @@
 #!/usr/bin/perl
 
+use strict;
+use warnings;
+
 use LWP::Simple;
+use JSON;
+use Data::Dumper;
 
-my $url = 'https://api.sunrise-sunset.org/json?lat=-33.79&lng=151.02';
+my $url = 'https://api.sunrise-sunset.org/json?lat=-33.79&lng=151.02&formatted=0';
+my $json = get $url || die "Couldn't get $url";
 
-my $content = get $url;
-die "Couldn't get $url" unless defined $content;
+my $hash = from_json($json);
 
-print "$content\n";
+print  Dumper($hash) . "\n";
+
+print $hash->{'results'} . "\n";
+
+print $hash->{'results'}{'sunset'} . "\n";
