ee74fb9d93430254e4b3b4a380b6aab97eaf486e
Mick <the8aron@yahoo.com>
2020-01-10 13:57:54 +1100

    sun rises and sets

--- Tidey.pl
+++ Tidey.pl
@@ -11,9 +11,14 @@ my $url = 'https://api.sunrise-sunset.org/json?lat=-33.79&lng=151.02&formatted=0
 my $json = get $url || die "Couldn't get $url";
 
 my $hash = from_json($json);
+die "error received in sunset/sunrise data.\n\n$json" unless $hash->{'status'} eq "OK";
 
 print  Dumper($hash) . "\n";
 
 print $hash->{'results'} . "\n";
 
 print $hash->{'results'}{'sunset'} . "\n";
+my $sunset  = $hash->{'results'}{'sunset'};
+my $sunrise = $hash->{'results'}{'sunrise'};
+
+print "Sunrise: $sunrise\tSunset: $sunset\n\n";
